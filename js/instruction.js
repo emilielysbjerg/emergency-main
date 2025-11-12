1c122507f8dae886a5a7ed988686b3fd6fc1d450
Emilie <Emilie>
2025-11-12 10:05:38 +0100

    popover

--- js/instruction.js
+++ js/instruction.js
@@ -1,15 +1,53 @@
 console.log("Loaded instructions...");
 
-document.querySelector("#cirkel_nede").addEventListener("click", vandmelonF);
+document.querySelector("#cirkel_top").addEventListener("click", vandmelonF);
 
-document.querySelector("#cirkel_midte").addEventListener("click", vandmelonF);
-
-document.querySelector("#cirkel_nede").addEventListener("click", vandmelonF);
-
+//øverste cirkel start*//
 function vandmelonF() {
-  console.log("glad.vandmelon");
+  console.log("vandmelonf kører");
+  document.querySelector(".circle_top ").classList.add("my_scale");
+  document.querySelector(".info-text .placeholder").textContent =
+    "Her skal stå tekst";
+}
+//øverste cirkel slut*//
+
+//midte cirkel start*//
+function midteF() {
+  console.log("vandmelonf kører");
+  document.querySelector(".circle_mid").classList.add("my_scale");
+  document.querySelector(".info-text .placeholder").textContent =
+    "Her skal stå tekst";
+}
+//midte cirkel slut*//
+
+//nederste cirkel start*//
+function bundF() {
+  console.log("vandmelonf kører");
+  document.querySelector(".circle_bottom").classList.add("my_scale");
+  document.querySelector(".info-text .placeholder").textContent =
+    "Her skal stå tekst";
+}
+//nedesrte cirkel slut*//
+
+document.querySelector("#cirkel_midte").addEventListener("click", midteF);
+document.querySelector("#cirkel_bund").addEventListener("click", bundF);
+
+function midteF() {
+  console.log("midte kører");
   document.querySelector("#Hotspots").classList.add("my_scale");
-  document.querySelector("Hotspots").computedStyleMap.fill = "green";
+  document.querySelector(".info-text .placeholder").textContent =
+    "Her skal stå mere tekst";
 }
 
-document.querySelector(".info.text h2").textContent = "vandmelon";
+// document.querySelector("#cirkel_midte").addEventListener("click", vandmelonF);
+
+// document.querySelector("#cirkel_nede").addEventListener("click", vandmelonF);
+
+// function vandmelonF() {
+//   console.log("glad.vandmelon");
+//   document.querySelector("#Hotspots").classList.add("my_scale");
+//   document.querySelector("#Hotspots").computedStyleMap.fill = "green";
+
+//}
+
+//document.querySelector(".info.text h2").textContent = "vandmelon";
